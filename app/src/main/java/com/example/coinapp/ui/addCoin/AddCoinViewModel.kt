252c91de756fa4f7c60c130ece2c9501f05d893c
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-05-04 23:37:08 +0200

    placeholder data updated

--- app/src/main/java/com/example/coinapp/ui/addCoin/AddCoinViewModel.kt
+++ app/src/main/java/com/example/coinapp/ui/addCoin/AddCoinViewModel.kt
@@ -7,22 +7,22 @@ import com.example.coinapp.data.Coin
 
 class AddCoinViewModel : ViewModel() {
     private val _items = MutableLiveData<List<Coin>>().apply { value = listOf(
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
-        Coin("BTC", 2),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
+        Coin("BTC", 50000),
     ) }
 
     val items: LiveData<List<Coin>>
