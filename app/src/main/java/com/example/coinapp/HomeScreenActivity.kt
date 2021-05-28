39e27def23ecd3907813c33334a78b150aad3001
Michal Vlcek <xvlcek3@fi.muni.cz>
2021-05-28 23:11:14 +0200

    added logout button to homescreen

--- app/src/main/java/com/example/coinapp/HomeScreenActivity.kt
+++ app/src/main/java/com/example/coinapp/HomeScreenActivity.kt
@@ -1,10 +1,17 @@
 package com.example.coinapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coinapp.ui.homeScreen.HomeScreenFragment
 
 class HomeScreenActivity : AppCompatActivity() {
+
+    companion object {
+        const val LOGIN = "login"
+        const val USER_ID = "user_id"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +20,25 @@ class HomeScreenActivity : AppCompatActivity() {
                 .replace(android.R.id.content, HomeScreenFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.logout_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                logout()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun logout() {
+
     }
 }
